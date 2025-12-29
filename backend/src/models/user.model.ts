b213d00294af04f0d376966661a3e357019de6ea
aleks1303 <aleksbulda13@gmail.com>
2025-12-29 23:15:52 +0200

    lesson 9 upload files

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,8 +1,9 @@
+import path from "node:path";
+
 import { model, Schema } from "mongoose";
 
 import { RoleEnum } from "../enums/role.enum";
 import { IUser } from "../interfaces/user.interface";
-import path from 'node:path';
 
 const userSchema = new Schema(
     {
@@ -17,7 +18,7 @@ const userSchema = new Schema(
         name: { type: String, required: true },
         surname: { type: String, required: true },
         age: { type: String, required: true },
-        avatar: {type: String, default: ''},
+        avatar: { type: String, default: "" },
         isActive: { type: Boolean, default: true },
         isDeleted: { type: Boolean, default: false },
         isVerified: { type: Boolean, default: false },
@@ -27,13 +28,13 @@ const userSchema = new Schema(
         versionKey: false,
         toJSON: {
             transform: (doc, ret) => {
-                delete ret.password
+                delete ret.password;
                 if (ret.avatar) {
                     ret.avatar = `/media/${path.basename(ret.avatar)}`;
                 }
-                return ret
-            }
-        }
+                return ret;
+            },
+        },
     },
 );
 export const User = model<IUser>("user", userSchema);
