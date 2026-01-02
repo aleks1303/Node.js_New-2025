6bd34b4abd95fe6b099e98968074b98d1400f31d
aleks1303 <aleksbulda13@gmail.com>
2026-01-02 15:41:01 +0200

    lesson 10 pagination

--- backend/src/repositorie/user.repository.ts
+++ backend/src/repositorie/user.repository.ts
@@ -1,3 +1,5 @@
+import { FilterQuery } from "mongoose";
+
 import {
     IUser,
     IUserCreateDTO,
@@ -6,11 +8,38 @@ import {
 import { User } from "../models/user.model";
 
 class UserRepository {
-    public getAll(query: IUserQuery): Promise<[IUser[], number]> {
-        const skip = query.pageSize * (query.page - 1);
-        return Promise.all([
-            User.find().limit(query.pageSize).skip(skip),
-            User.countDocuments(),
+    public getAll(query: IUserQuery): Promise<any> {
+        // const skip = query.pageSize * (query.page - 1);
+        const filterObject: FilterQuery<IUser> = { isDeleted: false };
+
+        if (query.search) {
+            filterObject.$or = [
+                { name: { $regex: query.search, $options: "i" } },
+                { surname: { $regex: query.search, $options: "i" } },
+            ];
+        }
+        const orderObject = {};
+        if (query.order) {
+            if (query.order.startsWith("-")) {
+                orderObject[query.order.slice(1)] = -1;
+            } else {
+                orderObject[query.order] = 1;
+            }
+        }
+        //User.find(filterObject).limit(query.pageSize).skip(skip);
+        return User.aggregate([
+            { $match: filterObject },
+            { $sort: orderObject },
+            {
+                $group: {
+                    _id: null,
+                    totalItems: { $sum: 1 },
+                    data: { $push: "$$ROOT" },
+                },
+            },
+            {
+                $project: { _id: 0 },
+            },
         ]);
     }
     public create(user: IUserCreateDTO): Promise<IUser> {
