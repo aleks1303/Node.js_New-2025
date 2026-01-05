6fa090cb64502066b7d61d20e711acb7f492dc6e
aleks1303 <aleksbulda13@gmail.com>
2026-01-05 21:25:58 +0200

    lesson 11 cron

--- backend/src/crons/remove-old-tokens.cron.ts
+++ backend/src/crons/remove-old-tokens.cron.ts
@@ -1,6 +1,20 @@
-// const handler = async () => {
-//     try {
-//     } catch (e) {
-//         console.error(e.message);
-//     }
-// };
+import { CronJob } from "cron";
+
+import { config } from "../configs/config";
+import { timeHelper } from "../helpers/time.helper";
+import { tokenRepository } from "../repositorie/token.repository";
+
+const handler = async () => {
+    try {
+        const lifeTime = config.JWT_REFRESH_LIFETIME;
+        const { value, unit } = timeHelper.parseLifeTime(lifeTime);
+        const date = timeHelper.subFromCurrentTime(value, unit);
+        const count = await tokenRepository.deleteBeforeDate(date);
+        if (count) {
+            console.log(`Deleted ${count} old tokens`);
+        }
+    } catch (e) {
+        console.error(e.message);
+    }
+};
+export const removeOldTokensCron = new CronJob(" 0 * * * * *", handler);
